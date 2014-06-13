d02841642cb676690906b97d172a03ccd48cc56e
Tuuli Saarinen <saari.tuulinen@gmail.com>
2014-06-13 14:17:52 -0400

    Final map etc. in scene 1
    Final map etc.

---
+++ fleeting-moments/Assets/Scripts/ClickStart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickStart : MonoBehaviour {
+	public string levelToLoad = "01_scene";
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("splashScreen");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButtonDown(0))
+		{
+			Application.LoadLevel (levelToLoad);
+		}
+	}
+}
